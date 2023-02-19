250dd866aaf1bb81da376a3a57ee8cf783529255
yhaering <mail@yhaering.de>
2023-02-19 13:01:49 +0100

    chore(resolver): fixed unit tests

--- packages/runtime/glacier-resolver/src/functions/fs/isFilePath.test.ts
+++ packages/runtime/glacier-resolver/src/functions/fs/isFilePath.test.ts
@@ -18,15 +18,6 @@ describe('isFilePath', () => {
     expect(isFilePath('../test'));
   });
 
-  test('calls getRootPath if a path does not start with a dot', () => {
-    isFilePath('/a');
-    expect(getRootPath).toHaveBeenCalledWith('/a');
-  });
-
-  test('returns true if path starts with root path', () => {
-    expect(isFilePath('{{ROOT_PATH}}test')).toBe(true);
-  });
-
   test('otherwise return false', () => {
     expect(isFilePath('test')).toBe(false);
   });
